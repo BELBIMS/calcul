95c111224054984f2d67d070de4466fb103e3412
Душин Максим <60781555+BELBIMS@users.noreply.github.com>
2024-02-15 10:19:51 +0300

    Program.cs

--- a1/a1/Program.cs
+++ a1/a1/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 
 class Calculator<T>
 {
+    // Метод для сложения двух чисел
     public static T Add(T x, T y)
     {
         dynamic dx = x;
@@ -9,6 +10,7 @@ class Calculator<T>
         return dx + dy;
     }
 
+    // Метод для вычитания одного числа из другого
     public static T Subtract(T x, T y)
     {
         dynamic dx = x;
@@ -16,6 +18,7 @@ class Calculator<T>
         return dx - dy;
     }
 
+    // Метод для умножения двух чисел
     public static T Multiply(T x, T y)
     {
         dynamic dx = x;
@@ -23,6 +26,7 @@ class Calculator<T>
         return dx * dy;
     }
 
+    // Метод для деления одного числа на другое
     public static T Divide(T x, T y)
     {
         dynamic dx = x;
@@ -39,34 +43,34 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введите первое число:");
+        Console.WriteLine("Введите первое число:"); // Ввод первого числа
         dynamic num1 = Convert.ChangeType(Console.ReadLine(), typeof(double));
-        Console.WriteLine("Введите второе число:");
+        Console.WriteLine("Введите второе число:"); // Ввод второго числа
         dynamic num2 = Convert.ChangeType(Console.ReadLine(), typeof(double));
 
-        Console.WriteLine("Выберите операцию:");
+        Console.WriteLine("Выберите операцию:"); // Ввод операции
         char operation = char.Parse(Console.ReadLine());
 
         dynamic result = 0;
         switch (operation)
         {
             case '+':
-                result = Calculator<dynamic>.Add(num1, num2);
+                result = Calculator<dynamic>.Add(num1, num2); // Выполнение сложения
                 break;
             case '-':
-                result = Calculator<dynamic>.Subtract(num1, num2);
+                result = Calculator<dynamic>.Subtract(num1, num2); // Выполнение вычитания
                 break;
             case '*':
-                result = Calculator<dynamic>.Multiply(num1, num2);
+                result = Calculator<dynamic>.Multiply(num1, num2); // Выполнение умножения
                 break;
             case '/':
-                result = Calculator<dynamic>.Divide(num1, num2);
+                result = Calculator<dynamic>.Divide(num1, num2); // Выполнение деления
                 break;
             default:
                 Console.WriteLine("Неверная операция");
                 break;
         }
 
-        Console.WriteLine("Результат вычислений: " + result);
+        Console.WriteLine("Результат вычислений: " + result); // Вывод результата вычислений
     }
 }
